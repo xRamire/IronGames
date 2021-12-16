70a1c2c5838b0117bdeb6554445bfa8b85a8d0bf
Adrian Ramirez <ramiire@gmail.com>
2021-12-16 12:01:37 +0100

    deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-  app.use("/api", require("./auth.routes"));
+  app.use("/api/auth", require("./auth.routes"));
   app.use("/api/reviews", require("./review.routes"))
   app.use("/api/games", require("./game.routes"));
   app.use("/api/user", require("./user.routes"))
