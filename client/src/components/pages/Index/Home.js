176f0de170cd95ef5fd457303b2aee1b98b8d6a9
Adrian Ramirez <ramiire@gmail.com>
2023-08-03 20:42:17 +0200

    changes

--- client/src/components/pages/Index/Home.js
+++ client/src/components/pages/Index/Home.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import ironlogo from '../../../assets/images/ironhack-1.svg'
 import './Home.css'
 
 const Home = () => {
     return (
         <>
-            <div className="main-background">
-                <Container>
-                    <img className="main-logo" src={ironlogo} alt="logo" ></img>
+            <section className="landing">
 
-                    <div className="main-text">
-                        <h1>Welcome to IronGames</h1>
-                        <p>Discover what ironhackers made in the third week of IronHack Web-Dev course!</p>
-                    </div>
+                <Container>
+                    <Row>
+                        <div className="main-background">
+                            {/* <img className="main-logo" src={ironlogo} alt="logo" ></img> */}
+
+                            <div className="main-text">
+                                <h1>Welcome to IronGames</h1>
+                                <p>Discover what ironhackers made in the third week of IronHack Web-Dev course!</p>
+                            </div>
+                        </div>
+                    </Row>
                 </Container>
-            </div>
+                <div className="home-bg">
+
+                </div>
+
+            </section>
+
         </>
     )
 }
