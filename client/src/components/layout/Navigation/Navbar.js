2fd61627c2f90caae5a1b22eff942dba7f84e230
Adrian Ramirez <ramiire@gmail.com>
2023-08-03 18:50:41 +0200

    fixes

--- client/src/components/layout/Navigation/Navbar.js
+++ client/src/components/layout/Navigation/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import AuthService from '../../../services/auth.service'
 import logo from '../../../assets/images/ironhack-1.svg'
@@ -15,33 +15,42 @@ const Navigation = ({ loggedUser, storeUser }) => {
     }
 
     return (
-        <nav>
 
+        <Nav className="nav">
             <Container>
-                <div className='title-logo'>
-                    <img className='logo' src={logo} alt='logo'></img>
-                    <Navbar.Brand className='title' as={Link} to='/'>IronGames</Navbar.Brand>
-                </div>
-                <Nav className="nav">
-                    <Nav.Link className='link' as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link className='link' as={Link} to="/game-list">Videogames</Nav.Link>
-                    <Nav.Link className='link' as={Link} to="/user-list">Users</Nav.Link>
+                <Row className='h-100 align-items-center'>
+                    <Col>
+                        <div className='title-logo h-100'>
+                            <img className='logo' src={logo} alt='logo'></img>
+                            <Navbar.Brand className='title' as={Link} to='/'>IronGames</Navbar.Brand>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className='d-flex h-100 justify-content-end'>
+                            <Nav.Link className='link' as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link className='link' as={Link} to="/game-list">Videogames</Nav.Link>
+                            <Nav.Link className='link' as={Link} to="/user-list">Users</Nav.Link>
 
-                    {loggedUser ?
-                        <>
-                            <Nav.Link className='link' as={Link} to="/my-profile">Profile</Nav.Link>
-                            <Nav.Link className='link' as={Link} to='/' onClick={logout}>Logout</Nav.Link>
-                        </>
-                        :
-                        <>
-                            <Nav.Link className='link' as={Link} to="/signup">Signup</Nav.Link>
-                            <Nav.Link className='link' as={Link} to="/login">Login</Nav.Link>
+                            {loggedUser ?
+                                <>
+                                    <Nav.Link className='link' as={Link} to="/my-profile">Profile</Nav.Link>
+                                    <Nav.Link className='link' as={Link} to='/' onClick={logout}>Logout</Nav.Link>
+                                </>
+                                :
+                                <>
+                                    <Nav.Link className='link' as={Link} to="/signup">Signup</Nav.Link>
+                                    <Nav.Link className='link' as={Link} to="/login">Login</Nav.Link>
 
-                        </>
-                    }
-                </Nav>
+                                </>
+                            }
+                        </div>
+                    </Col>
+                </Row>
             </Container>
-        </nav>
+        </Nav>
+
+
+
     )
 }
 
